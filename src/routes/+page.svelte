ab224795af382eb63ebb35bf57885210ae9ce320
Nathan James <nathanjms@outlook.com>
2023-11-05 19:14:05 +0000

    test adding some JS

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,16 @@
+<script>
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		console.log('mounted');
+	});
+
+	$: console.log($page);
+</script>
+
 <h1 class="text-3xl">Welcome to SvelteKit</h1>
+<pre><code
+		>{JSON.stringify($page, null, 2)}
+    </code>
+</pre>
